64b1e3bd3ceed4eb428f4329fa0ae107b85bb810
Thomas <thomas.conquisti@gmail.com>
2025-04-26 18:53:41 -0300

    .

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -8,28 +8,28 @@ storiesOf('Button', module)
     .add('base', () => `
         <div class="slds-p-around_medium">
             <c-button label="Base Button"></c-button>
-        </div>    
+        </div>
     `)
     .add('neutral', () => `
         <div class="slds-p-around_medium">
             <c-button variant="neutral" label="Neutral Button"></c-button>
-        </div>    
+        </div>
     `)
     .add('brand', () => `
         <div class="slds-p-around_medium">
             <c-button variant="brand" label="Brand Button"></c-button>
-        </div>    
+        </div>
     `)
     .add('destructive', () => `
         <div class="slds-p-around_medium">
             <c-button variant="destructive" label="Destructive Button"></c-button>
-        </div>    
+        </div>
     `)
     .add('success', () => `
         <div class="slds-p-around_medium">
             <c-button variant="success" label="Success Button"></c-button>
-        </div>    
+        </div>
     `);
 
 
-   
+
